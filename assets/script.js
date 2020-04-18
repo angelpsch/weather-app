b2f9b6ffb7a14a580948b8b0347810d180febbd3
angelpsch <angelpsch@users.noreply.github.com>
2020-04-18 08:49:16 -0500

    -A

--- assets/script.js
+++ assets/script.js
@@ -43,9 +43,11 @@ var year = currentDate.getFullYear();
         console.log(Fahrenheit); 
         
         cityTitle.text(response.name + " " + (month + 1) + "/" + today + "/" + year);
-        tempDisplay.text(Fahrenheit + "°F");
-        humidityDisplay.text(response.main.humidity + "%");
-        windDisplay.text(response.wind.speed + ' MPH'); 
+        tempDisplay.val(" " + Fahrenheit + "°F");
+        humidityDisplay.val(" " + response.main.humidity + "%");
+        windDisplay.val(" " + response.wind.speed + ' MPH'); 
+
+        localStorage.setItem('cityName', response.name); 
         
         var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
           $.ajax({
@@ -56,17 +58,51 @@ var year = currentDate.getFullYear();
         var uvIndex = $('#uv-badge');
        
         if (response.value <= 5) {
-            uvIndex.attr('class', 'badge-success');
+            uvIndex.attr('class', 'badge-success rounded-pill p-1');
         } else if (response.value >= 5.01 && response.value < 8.00){
-            uvIndex.attr('class', 'badge-warning');
+            uvIndex.attr('class', 'badge-warning rounded-pill p-1');
         } else {
-            uvIndex.attr('class', 'badge-danger');
+            uvIndex.attr('class', 'badge-danger rounded-pill p-1');
         } 
         uvIndex.text(response.value); 
   
       });
+        var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + 'chicago' + '&appid=' + APIKey;
+          $.ajax({
+        url: fiveDayURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        
+        var list = response.list; 
+        console.log(list); 
+        $(list).each(function(index){
+          today++;
+          var K = parseInt(list[index].main.temp);
+          var F =  (K-273.15)*(9/5) + 32; 
+          F = Math.round(F); 
+          var divCol = $('<div class="col-2 text-center">'); 
+          $('#future-forcast').append(divCol); 
+          var card = $('<div class="card badge-info">');   
+          divCol.append(card);
+          var cardBody = $('<div class="card-body">');
+          card.append(cardBody); 
+          var dateHeader = $('<h5 class="card-title">');
+          dateHeader.text((month+1) + '/' + today + '/' + year);   
+          cardBody.append(dateHeader);
+          var tempDisplay = $('<p>'); 
+          tempDisplay.text("Temperature: " + F + "°F"); 
+          cardBody.append(tempDisplay); 
+
+
+          if (index >= 5){
+            return false; 
+          }
+          console.log(index); 
+        })
       });
-      
+      });
+       
       
 
 //    })
