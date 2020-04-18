d73713df43139aaf54f283db961cb9f3abf6dfb4
angelpsch <angelpsch@users.noreply.github.com>
2020-04-18 09:59:23 -0500

    -A

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
+renderCities(); 
+
 // Declare Variables
-var searchBtn = $('#search-btn');
+var searchBtn = $('.search-btn');
 var cityList = $('#city-list');
 var cityTitle = $('#city-title'); 
 var tempDisplay = $('#temperature');
@@ -20,12 +22,12 @@ var year = currentDate.getFullYear();
     var APIKey = "02d888ba45e0cc8be097f82b4ce2058d"; 
 
 
-//    searchBtn.click(function(){
+searchBtn.click(function(){
        cityInput = $('#city-input').val().trim().toLowerCase(); 
        console.log(cityInput)
-
+      var cityName = []; 
        
-        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "chicago"  + "&appid=" + APIKey;
+        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput  + "&appid=" + APIKey;
           $.ajax({
         url: weatherURL,
         method: "GET"
@@ -47,7 +49,9 @@ var year = currentDate.getFullYear();
         humidityDisplay.val(" " + response.main.humidity + "%");
         windDisplay.val(" " + response.wind.speed + ' MPH'); 
 
-        localStorage.setItem('cityName', response.name); 
+        cityName.push(response.name); 
+        console.log(cityName); 
+       
         
         var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
           $.ajax({
@@ -93,8 +97,9 @@ var year = currentDate.getFullYear();
           var tempDisplay = $('<p>'); 
           tempDisplay.text("Temperature: " + F + "°F"); 
           cardBody.append(tempDisplay); 
-
-
+          var humDisplay = $('<p>');
+          humDisplay.text('Humidity: ' + list[index].main.humidity + '%'); 
+          cardBody.append(humDisplay); 
           if (index >= 5){
             return false; 
           }
@@ -103,7 +108,33 @@ var year = currentDate.getFullYear();
       });
       });
        
-      
+    localStorage.setItem('key', JSON.stringify(cityName)); 
+    renderCities(); 
+   }); 
 
-//    })
+function renderCities(){ 
+  $('#city-list').empty(); 
+  var cities = [];
+  cities.push(localStorage.getItem(JSON.parse('key'))); 
+  console.log(cities); 
+  $(cities).each(function(index){ 
+    var city = cities[index]; 
+    if(city !== null){
+      var cityName = $('<button class="search-btn border-0 badge-info">'); 
+      cityName.text(city);
+      $('#city-list').append(cityName); 
+      cityInput = city; 
+        if (index >= 5){
+          return false; 
+        }
+  } else {
+    $('#city-list').text('You do not have any saved cities yet.'); 
+  }
+     }); 
+  
+
+}
+
+
+
  
